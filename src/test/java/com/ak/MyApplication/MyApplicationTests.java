2f1f81c31676333b5e54807b3ccfeb2bba738374
Atharva Kanojia <kanojiaatharva@gmail.com>
2021-08-09 19:45:58 +0530

    first commit

---
+++ src/test/java/com/ak/MyApplication/MyApplicationTests.java
@@ -0,0 +1,38 @@
+package com.ak.MyApplication;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MyApplicationTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@InjectMocks
+    private GreetingController greetingController;
+	
+		
+	@Test
+    public void testgreeting() throws Exception {
+
+        mockMvc.perform(get("/greeting"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().string("{\"id\":1,\"content\":\"Hello, World!\"}"));
+      
+    }
+
+}
